ed698454e48dee39da6f40d98c6b1ec20a9b9fbb
ibrohim <ibrohim>
2024-06-26 15:32:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var yosh =+prompt("Yoshingizni kiriting")
+if (yosh<=18) {
+    alert("Siz hali yoshsiz oqishingiz kerak")
+}
+
+ else if (yosh<=50) {
+    alert("ishlashingiz kerak")
+}
+else if(yosh<=59){
+    alert("Siz yaqinda pensiyaga chiqasiz")
+}
+else if (yosh<=150) {
+    alert("Siz pensiyanerga oxshaysiz, hali tirik bolsangiz ")
+}
+else{
+    alert("Nimadir notogri ketib qoldi")
+}
